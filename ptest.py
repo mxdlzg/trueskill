c32de8c6528d9fee57684499466331bd86b7515a
Wei Tingjiang <mxdlzg@163.com>
2022-02-27 19:19:56 +0800

    Update ptest.py

--- ptest.py
+++ ptest.py
@@ -1,13 +1,13 @@
-import _trueskill
+import trueskill_cpp
 from pprint import pprint
 from timeit import timeit
 
 players = []
-for i in range(4):
+for i in range(2):
     players.append((25., 25./3., i+1))
 
 def test():
-    return _trueskill.adjust_players(players)
+    return trueskill_cpp.adjust_players(players)
 
 ITER = 1000000
 print('{} iterations in {}s'.format(ITER, timeit(test, number=ITER)))
